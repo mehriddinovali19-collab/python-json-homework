a73a0b82bde4ab443a12e4acd3a874f6d045a921
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-28 14:19:45 +0500

    task03 done

--- task03.py
+++ task03.py
@@ -1,4 +1,21 @@
-# 1 - funksiya yaratamiz 
-# 2 - har bir userni summasni jammisini topamiz
-# 3 - yangi structurega qaytaramiz 
+import json
+def is_valid_user(user: dict) -> bool:
+    return user["name"] != " " and "@" in user["email"] and user["age"] > 0
 
+def split_users(data: list) -> dict:
+        valid_user = []
+        not_valid_user = []
+        for user in data:
+             if is_valid_user(user):
+                valid_user.append(user)
+             else:
+                  not_valid_user.append(user)
+        return {"valid user": valid_user,
+                "not valid user": not_valid_user}
+data = """[
+  {"name": "Ali", "email": "ali@example.com", "age": 20},
+  {"name": "", "email": "invalid-email", "age": -5},
+  {"name": "Vali", "email": "vali@example.com", "age": 25}
+]"""
+parsed_data = json.loads(data)
+print(split_users(parsed_data))
