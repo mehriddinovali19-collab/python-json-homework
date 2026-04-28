834d0a05310f3a7776debd22730e1e122b8d2eb7
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-28 13:36:11 +0500

    task02 done

--- task02.py
+++ task02.py
@@ -1,5 +1,29 @@
-# 1. JSON → Python
-# 2. Faqat is_student=True bo‘lganlar
-# 3. Soni
-# 4. O‘rtacha yosh
-# 5. Natija
+import json
+def calculate_total_spent(data: dict) -> list:
+    kilent = json.loads(data)
+    result = []
+    for user in kilent["users"]:
+        total = sum(order["price"] for order in user["orders"])
+        result.append([
+  {"id": user['id'], "name": user['name'], "total_spent": total}
+])
+    return result 
+data = """{
+  "users": [
+    {
+      "id": 1,
+      "name": "Ali",
+      "orders": [
+        {"id": 101, "price": 100},
+        {"id": 102, "price": 150}
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Vali",
+      "orders": []
+    }
+  ]
+}"""
+print(calculate_total_spent(data))
+    
--- task03.py
+++ task03.py
@@ -1,1 +1,4 @@
+# 1 - funksiya yaratamiz 
+# 2 - har bir userni summasni jammisini topamiz
+# 3 - yangi structurega qaytaramiz 
 
